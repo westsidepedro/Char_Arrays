8a9cc3e92614950938bbaa4f8cfb3d61507c2b6d
jpetterson <jpetterson@DS-17.decker.local>
2018-02-07 12:04:34 -0600

    2-07-18

---
+++ src/Char_Arrays.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Char_Arrays.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello there Sexy 1238!" << endl; // prints Hello there Sexy!
+	return 0;
+}
